cd7433d8b6612761fd735567b1dfe229a060cbf2
Angelika Tyborska <atyborska93@gmail.com>
2015-12-18 14:14:55 +0100

    Add FactoryGirl to RSpec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
