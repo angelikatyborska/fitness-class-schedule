26b3d6ff38aa78bdf4760d60519063a27c7fb136
Angelika Tyborska <atyborska93@gmail.com>
2015-12-19 17:16:40 +0100

    Add checking for full schedule items to specs

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -7,9 +7,23 @@ RSpec.describe Reservation do
 
     context 'with schedule item starting in the past' do
       subject { build(:reservation, schedule_item: build(:schedule_item, start: DateTime.now - 1.day ))}
+
       it 'is invalid' do
         is_expected.to be_invalid
         expect(subject.errors[:schedule_item]).to include('can\'t make reservations for items from the past')
+      end
+    end
+
+    context 'with schedule_items that can\'t accept any more users' do
+      before :each do
+        @full_schedule_item = create(:schedule_item, capacity: 1)
+        create(:reservation, schedule_item: @full_schedule_item)
+      end
+
+      subject { build(:reservation, schedule_item: @full_schedule_item) }
+      it 'is invalid' do
+        is_expected.to be_invalid
+        expect(subject.errors[:schedule_item]).to include('schedule_item capacity exceeded')
       end
     end
   end
