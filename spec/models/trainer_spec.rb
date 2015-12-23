051715ac00b2f30d3ef27302d6d099c1317ad3e0
Angelika Tyborska <atyborska93@gmail.com>
2015-12-23 13:41:00 +0100

    Extract occupiable spec into separate file

--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Trainer do
+  it_behaves_like 'occupiable', :trainer
+
   describe 'validations' do
     it { is_expected.to validate_presence_of :first_name }
     it { is_expected.to validate_presence_of :description }
