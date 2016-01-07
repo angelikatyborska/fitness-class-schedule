f7d54780aab8b6b87281e087724fe56207010f8b
Angelika Tyborska <atyborska93@gmail.com>
2016-01-07 14:53:11 +0100

    Make the whole item box an anchor

--- spec/features/schedules/guest_checks_schedules_for_rooms_spec.rb
+++ spec/features/schedules/guest_checks_schedules_for_rooms_spec.rb
@@ -3,18 +3,44 @@ require 'rails_helper'
 feature 'Guest check schedules for rooms' do
   let!(:small_room) { create(:room, name: 'Small room') }
   let!(:big_room) { create(:room, name: 'Big room') }
-  let!(:small_schedule_item_this_week) {
-    create(:schedule_item, room: small_room, activity: 'ABT', start: Time.zone.parse('8:05'), capacity: 6 )
+  let!(:small_schedule_item_this_monday) do
+    create(
+      :schedule_item,
+      room: small_room,
+      activity: 'ABT',
+      start: Time.zone.now.beginning_of_week + 8.hours + 5.minutes,
+      capacity: 6
+    )
+  end
+
+  let!(:small_schedule_item_next_monday) do
+    create(
+      :schedule_item,
+      room: small_room,
+      activity: 'Step',
+      start: Time.zone.now.beginning_of_week + 1.week + 8.hours + 15.minutes,
+      capacity: 6 )
+  end
+
+  let!(:big_schedule_item_this_sunday) {
+    create(
+      :schedule_item,
+      room: big_room,
+      activity: 'TBC',
+      start: Time.zone.now.beginning_of_week + 6.days + 9.hours + 5.minutes,
+      capacity: 20
+    )
   }
-  let!(:small_schedule_item_next_week) {
-    create(:schedule_item, room: small_room, activity: 'Step', start: Time.zone.parse('8:15') + 7.days, capacity: 6 )
-  }
-  let!(:big_schedule_item_this_week) {
-    create(:schedule_item, room: big_room, activity: 'TBC', start: Time.zone.parse('9:05'), capacity: 20)
-  }
-  let!(:big_schedule_item_next_week) {
-    create(:schedule_item, room: big_room, activity: 'Step', start: Time.zone.parse('9:15') + 7.days, capacity: 20)
-  }
+
+  let!(:big_schedule_item_next_sunday) do
+    create(
+      :schedule_item,
+      room: big_room,
+      activity: 'Step',
+      start: Time.zone.now.beginning_of_week + 1.week + 6.days + 9.hours + 15.minutes,
+      capacity: 20
+    )
+  end
 
   before :all do
     Timecop.freeze(Time.zone.now.beginning_of_week)
@@ -24,61 +50,65 @@ feature 'Guest check schedules for rooms' do
     Timecop.return
   end
 
-  scenario 'all rooms' do
-    visit root_path
-    expect(page).to have_content 'ABT'
-    expect(page).to have_content '8:05'
-    expect(page).to have_content 'TBC'
-    expect(page).to have_content '9:05'
+  context 'at the beginning of the week' do
+    scenario 'all rooms' do
+      visit root_path
+      expect(page).to have_link 'ABT'
+      expect(page).to have_content '8:05'
+      expect(page).to have_link 'TBC'
+      expect(page).to have_content '9:05'
 
-    click_link 'Next week'
+      click_link 'Next week'
 
-    expect(page).to have_content 'Step'
-    expect(page).to have_content '8:15'
-    expect(page).to have_content '9:15'
+      expect(page).to have_link 'Step'
+      expect(page).to have_content '8:15'
+      expect(page).to have_content '9:15'
+    end
+
+    scenario 'the small room' do
+      visit root_path
+      click_link 'Rooms'
+      click_link 'Small room'
+
+      expect(page).to have_link 'ABT'
+      expect(page).to have_content '8:05'
+
+      expect(page).not_to have_content 'TBC'
+      expect(page).not_to have_content '9:05'
+
+      click_link 'Next week'
+
+      expect(page).to have_link 'Step'
+      expect(page).to have_content '8:15'
+      expect(page).not_to have_content '9:15'
+    end
+
+    scenario 'the big room' do
+      visit root_path
+      click_link 'Rooms'
+      click_link 'Big room'
+
+      expect(page).not_to have_content 'ABT'
+      expect(page).not_to have_content '8:05'
+
+      expect(page).to have_link 'TBC'
+      expect(page).to have_content '9:05'
+
+      click_link 'Next week'
+
+      expect(page).to have_link 'Step'
+      expect(page).not_to have_content '8:15'
+      expect(page).to have_content '9:15'
+    end
   end
 
-  scenario 'the small room' do
-    visit root_path
-    click_link 'Rooms'
-    click_link 'Small room'
-
-    expect(page).to have_content 'ABT'
-    expect(page).to have_content '8:05'
-
-    expect(page).not_to have_content 'TBC'
-    expect(page).not_to have_content '9:05'
-
-    click_link 'Next week'
-
-    expect(page).to have_content 'Step'
-    expect(page).to have_content '8:15'
-    expect(page).not_to have_content '9:15'
-  end
-
-  scenario 'the big room' do
-    visit root_path
-    click_link 'Rooms'
-    click_link 'Big room'
-
-    expect(page).not_to have_content 'ABT'
-    expect(page).not_to have_content '8:05'
-
-    expect(page).to have_content 'TBC'
-    expect(page).to have_content '9:05'
-
-    click_link 'Next week'
-
-    expect(page).to have_content 'Step'
-    expect(page).not_to have_content '8:15'
-    expect(page).to have_content '9:15'
-  end
-
-  scenario 'all rooms in the future' do
-    Timecop.freeze(Time.zone.now.end_of_week) do
+  context 'on a wednesday' do
+    scenario 'all rooms' do
+      Timecop.freeze(Time.zone.now.beginning_of_week + 3.days)
       visit root_path
       expect(page).to have_content 'ABT'
       expect(page).to have_no_link 'ABT'
+      expect(page).to have_link 'TBC'
     end
   end
 end
