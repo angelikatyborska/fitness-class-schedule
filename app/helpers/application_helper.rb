f08c7b31d0254293ba3016777270134e929d8b67
Angelika Tyborska <atyborska93@gmail.com>
2016-01-15 09:07:20 +0100

    Refactor ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
   def schedule_item_width(item, schedule_items)
-    n = schedule_items.count { |other_item| other_item.going_on_between?(item.start, item.stop) }
+    n = schedule_items.count do |other_item|
+      other_item.going_on_between?(item.start, item.stop)
+    end
 
     100.0 / n
   end
 
   def schedule_item_left_position(item, schedule_items)
-    all_items_at_that_time = schedule_items.select { |other_item| other_item.going_on_between?(item.start, item.stop) }
+    all_items_at_that_time = schedule_items.select do |other_item|
+      other_item.going_on_between?(item.start, item.stop)
+    end
 
     i = all_items_at_that_time.find_index { |other_item| other_item == item}
 
@@ -19,11 +23,19 @@ module ApplicationHelper
 
 
   def schedule_item_start_day_percentage(item)
-    (item.start.in_website_time_zone - ScheduleItem.beginning_of_day(item.start.in_website_time_zone)) / (ScheduleItem.end_of_day(item.start.in_website_time_zone) - ScheduleItem.beginning_of_day(item.start.in_website_time_zone)) * 100
+    start = item.start.in_website_time_zone
+    day_start = ScheduleItem.beginning_of_day(start)
+    day_end = ScheduleItem.end_of_day(start)
+
+    (start - day_start) / (day_end - day_start) * 100
   end
 
   def schedule_item_duration_day_percentage(item)
-    (item.start.in_website_time_zone + item.duration.minutes - item.start.in_website_time_zone) / (ScheduleItem.end_of_day(item.start.in_website_time_zone) - ScheduleItem.beginning_of_day(item.start.in_website_time_zone)) * 100
+    start = item.start.in_website_time_zone
+    day_start = ScheduleItem.beginning_of_day(start)
+    day_end = ScheduleItem.end_of_day(start)
+
+    (start + item.duration.minutes - start) / (day_end - day_start) * 100
   end
 
   def order_by_weekdays(schedule_items, week_offset)
